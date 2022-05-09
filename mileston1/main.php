6c4aecf039b4a381922f74a4caaff5ee930fb0ac
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-09 16:56:55 +0200

    fine mileston 1

--- mileston1/main.php
+++ mileston1/main.php
@@ -1,62 +1,70 @@
-<div class="container mt-3 sfondo_container ">
-    <div class="row row-cols-6">
-        <?php
-        
-            var_dump($database);
-       
-        
-        ?>
-        
-    </div>
-
-</div>
 <?php
-$database = [
+$dischi= [
     [
         'title' => 'New Jersey',
         'author' => 'Bon Jovi',
         'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'poster' => '<img class="img-fluid" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" >',
         'genre' => 'Rock'
     ],
     [
         'title' => 'Live at Wembley 86',
         'author' => 'Queen',
         'year' => 1992,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'poster' => '<img class="img-fluid" src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" >',
         'genre' => 'Pop'
     ],
     [
         'title' => 'Ten\'s Summoner\'s Tales',
         'author' => 'Sting',
         'year' => 1993,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'poster' => '<img class="img-fluid" src="https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg" >',
         'genre' => 'Pop'
     ],
     [
         'title' => 'Steve Gadd band',
         'author' => 'Steve Gadd Band',
         'year' => 2018,
-        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'poster' => '<img class="img-fluid" src="https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg" >',
         'genre' => 'Jazz'
     ],
     [
         'title' => 'Brave new World',
         'author' => 'Iron Maiden',
         'year' => 2000,
-        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'poster' => '<img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg" >',
         'genre' => 'Metal'
     ],
     [
         'title' => 'One more car, one more rider',
         'author' => 'Eric Clapton',
         'year' => 2002,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'poster' => '<img class="img-fluid" src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" >',
         'genre' => 'Rock'
     ]
 ];
-
-
-
-
 ?>
+
+
+<div class="container mt-3 sfondo_container ">
+    <div class="row row-cols-6">
+        <?php
+        foreach ($dischi as $value) {
+           echo "<div class='text-center col mx-3 my-3 p-3'>".
+           $value['poster'].
+           "<h4 class='text-uppercase mt-3'>".$value['title']."</h4>".
+           "<p>".$value['author']."</p>".
+           "<span>".$value['year']."</span>
+       </div>";
+        }
+        
+        ?>
+        <!-- <div class="text-center col mx-3 my-3 p-3">
+            <img src="img" alt="" class="img-fluid">
+            <h4 class="text-uppercase mt-3"></h4>
+            <p></p>
+            <span></span>
+        </div> -->
+    </div>
+
+</div>
