19e31f31f9ac28a287a28959943a0bc995b10b2c
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-10 10:22:16 +0200

    modifiche

--- milestone2/index.php
+++ milestone2/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style2.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
@@ -13,17 +13,20 @@
 
 <body>
     <div id="app">
-        <div class='container'>
-            <div class='row row-cols-6' >
-                <div class="text-center col mx-3 my-3 p-3 carta" v-for="element in array">
-                    <img :src='element.poster' alt="" class="img-fluid">
-                    <h4 class="text-uppercase mt-3">{{element.title}}</h4>
-                    <p>{{element.author}}</p>
-                    <span>{{element.year}}</span>
+        <div class='cont'>
+            <div class='container'>
+                <div class='row row-cols-4 justify-content-between'>
+                    <div class="text-center col mx-3 mb-3 p-3 carta" v-for="element in array">
+                        <img :src='element.poster' alt="" class="img-fluid">
+                        <h4 class="text-uppercase mt-3">{{element.title}}</h4>
+                        <p>{{element.author}}</p>
+                        <span>{{element.year}}</span>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
+
 
     </div>
 
